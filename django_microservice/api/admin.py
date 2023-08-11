f259bf3d9b6c4b3b8907a50449510450086eae76
Roman <romanwn@mail.com>
2023-08-11 12:52:51 +0300

    delete old

--- django_microservice/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Users
-from django.contrib.auth.models import Group
-
-admin.site.register(Users)
-admin.site.unregister(Group)
-
-admin.site.site_header = "API admin"
